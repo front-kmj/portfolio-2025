889b52adddd9f3fd91a0d70d5dce9af1a982b8ee
KMJ-portfolio <test3655@naver.com>
2025-03-12 00:35:14 +0900

    경력기술서 추가가

--- js/js.js
+++ js/js.js
@@ -138,4 +138,29 @@ $(function(){
             }
         });
     });
+
+    //career
+    $(".career-btn").on("click", function(){
+        $(".career").addClass("on");
+        $(".page-dim").show();
+        $('html, body').css('overflow','hidden');
+    });
+    $(".career-close").on("click", function(){
+        $(".career").removeClass("on");
+        $(".page-dim").hide();
+        $('html, body').css('overflow','visible');
+    });
+    var listBox = $(".list-box"),
+        listBoxLength = $(".list-box").length,
+        pLine = $(".p-line li .num"),
+        pLineLength = $(".p-line").length;
+
+    for(var j=0;j<listBoxLength;j++){
+        listBox.eq(j).find(pLine).each(function(i){
+            i = i+1;
+            $(this).text(i + ".");
+            if(i == pLineLength+1){return false;}
+        });
+    };
 });
+
